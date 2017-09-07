3a4a36f6558670278bcfdd352339337730448d08
Lua <luanaa.fv@gmail.com>
2017-09-07 18:16:14 -0300

    fim de aula

--- aula20170906/ger3.c
+++ aula20170906/ger3.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+int main()
+{
+    int P=0, L, R, D, i;
 
+    for (i=0; i<3; i++)
+    {
+        printf("Insira um limite: ");
+        scanf("%d", &L);
+
+        printf("Jogando dados...\n");
+        int x = 1 + rand() %(6-1+1);
+        printf("Dado 1: %d\n", x);
+        int y = 1 + rand() %(6-1+1);
+        printf("Dado 1: %d\n", y);
+        int z = 1 + rand() %(6-1+1);
+        printf("Dado 1: %d\n", z);
+        R=x+y+z;
+        printf("Soma dos dados: %d\n\n", R);
+
+        if (L>11)
+        {
+            D=L-10;
+            if (R>L)
+                P=P-(D*(R-L));
+            else
+                P=P+((L-R)/D)+1;
+        }
+        else
+            P=P+(11-R);
+
+    }
+    printf("\nPontos finais: %d\n", P);
+
+    return 0;
+}
--- aula20170906/ite2.c
+++ aula20170906/ite2.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int b, p, i, r=1;
+    printf("Insira a base e a potencia: ");
+    scanf("%d", &b);
+    scanf("%d", &p);
+    for(i=0; i<p; i++)
+        r=r*b;
+    printf("\nResultado: %d", r);
 
+    return 0;
+}
