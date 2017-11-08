85d3c44b1a8436546c775b44285c1724403dab7b
Lua-Ferreira <luanaa.fv@gmail.com>
2017-11-08 11:14:43 -0200

    fim de aula

--- aula20170906/ger1.c
+++ aula20170906/ger1.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main()
 {
     int numero, i;
     printf("Entre com um numero: ");
     scanf("%d", &numero);
     getchar();
-    for (i=2; i<numero; i++);
-    {
+    for (i=2; i<numero; i++)
         if(numero%i==0)
-            break;
-    }
+        break;
     if (i==numero)
         printf("Primo\n");
     else
